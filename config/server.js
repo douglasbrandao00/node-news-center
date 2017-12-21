a74c440764ddcfee2287c945be2c8d2e71f6a179
douglasbrandao00 <douglaswfbrandao@gmail.com>
2017-12-21 16:35:50 +0000

    adcionando express-validator

--- config/server.js
+++ config/server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const consign = require('consign');
 const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.set('view engine', 'ejs');
 app.set('views', './app/views');
 
 app.use(bodyParser.urlencoded({extended : true}));
+app.use(expressValidator())
 
 consign()
   .include('app/routes')
