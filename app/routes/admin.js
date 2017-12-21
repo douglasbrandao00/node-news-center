3b0295f17c7287896fcccc62bb68b4fdee96760f
douglasbrandao00 <douglaswfbrandao@gmail.com>
2017-12-21 17:27:01 +0000

    aplicando o express validator

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -4,6 +4,11 @@ module.exports = (application) => {
   });
 
   application.post('/admin/save', (req, res) => {
+    req.assert('titulo'       , 'titulo é obrigatorio').notEmpty();
+    req.assert('resumo'       , 'resumo é obrigatorio').notEmpty().len(10, 100);
+    req.assert('autor'        , 'autor é obrigatorio').notEmpty();
+    req.assert('data_noticia' , 'data_noticia é obrigatorio').notEmpty();
+
 		const connection = application.config.dbConnection();
 		let newsModel = new application.app.models.NoticiasDAO;
 
